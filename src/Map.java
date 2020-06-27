2ab5cc1f80f0e1604d1b0720c578e76413b10378
Ravi, Nishant <nravi34@gatech.edu>
2020-06-27 14:21:08 -0700

    Skeletal map class

--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,44 @@
-public class Map {
+import java.awt.*;
+import java.util.ArrayList;
 
+public class Map {
+    private ArrayList<Bus> buses = new ArrayList<>();
+    private ArrayList<Route> routes = new ArrayList<>();
+    private ArrayList<Stop> stops = new ArrayList<>();
+    private int width;
+    private int height;
+    private Color c;
+
+    public Map(int width, int height, Color c, ArrayList<Bus> buses, ArrayList<Route> routes, ArrayList<Stop> stops) {
+        this.width = width;
+        this.height = height;
+        this.c = c;
+        this.buses = buses;
+        this.routes = routes;
+        this.stops = stops;
+    }
+
+    public ArrayList<Bus> getBuses() {
+        return buses;
+    }
+
+    public ArrayList<Route> getRoutes() {
+        return routes;
+    }
+
+    public ArrayList<Stop> getStops() {
+        return stops;
+    }
+
+    public void addBus(Bus bus) {
+        buses.add(bus);
+    }
+
+    public void addRoute(Route route) {
+        routes.add(route);
+    }
+
+    public void addStop(Stop stop) {
+        stops.add(stop);
+    }
 }
