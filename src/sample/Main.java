103da5654538102c45c253b8edb135cac4953e64
Ravi, Nishant <nravi34@gatech.edu>
2020-06-27 13:56:15 -0700

    Update Main with helper methods for creating label

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,40 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.Cursor;
 
 public class Main extends Application {
 
@@ -19,5 +53,35 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public ImageView createImage(String path, int x, int y, int width, int height) {
+        Image newImage = new Image("\\resources\\" + path);
+        ImageView image = new ImageView(newImage);
+        image.setFitHeight(height);
+        image.setFitWidth(width);
+        image.setLayoutX(x);
+        image.setLayoutY(y);
+        return image;
+    }
+
+    public Label createLabel(String text, int x, int y, int font, Color c, int width) {
+        Label newLabel = new Label(text);
+        newLabel.setTextFill(c);
+        newLabel.setFont(new Font(font));
+        newLabel.setLayoutX(x);
+        newLabel.setLayoutY(y);
+        newLabel.setPrefWidth(width);
+        return newLabel;
+    }
+
+    public Slider createSlider(int min, int max, int val, int width, boolean ticks, int majorTick,
+                               int minorTick) {
+        Slider slider = new Slider(min, max, val);
+        slider.setPrefWidth(width);
+        slider.setShowTickMarks(ticks);
+        slider.setMajorTickUnit(majorTick);
+        slider.setMinorTickCount(minorTick);
+        return slider;
     }
 }
