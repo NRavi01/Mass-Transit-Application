7da9c00760a30a68c383c3514e381d4f67185e45
Ravi, Nishant <nravi34@gatech.edu>
2020-06-27 14:27:30 -0700

    MainScreen map and sidebar functionality

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,3 +1,15 @@
 public class MainScreen {
+    private Map map;
+    private String title;
+    private Sidebar sidebar;
 
+    public MainScreen(Map map, String title, Sidebar sidebar) {
+        this.map = map;
+        this.title = title;
+        this.sidebar = sidebar;
+    }
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
 }
