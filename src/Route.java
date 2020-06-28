e75eca4e9e021019defa61b990a275798354e86b
Ravi, Nishant <nravi34@gatech.edu>
2020-06-28 14:33:43 -0700

    Added route color

--- src/Route.java
+++ src/Route.java
@@ -1,9 +1,21 @@
+import javafx.scene.paint.Color;
+
 public class Route extends SimulationObject {
 
     private Stop[] stops;
+    private Color c;
 
-    public Route(String name, int id, Stop[] stops) {
+    public Route(String name, int id, Stop[] stops, Color c) {
         super(name, id);
         this.stops = stops;
+        this.c = c;
+    }
+
+    public Stop[] getStops() {
+        return stops;
+    }
+
+    public Color getColor() {
+        return c;
     }
 }
