3c4abfd3eb21699056d31e4cb829bd17367fa310
Ravi, Nishant <nravi34@gatech.edu>
2020-06-28 14:33:57 -0700

    Added stop location

--- src/Stop.java
+++ src/Stop.java
@@ -1,11 +1,21 @@
+import java.awt.*;
+
 public class Stop extends SimulationObject {
 
     private int numPassengers;
-    private Route route;
+    private Point location;
 
-    public Stop(String name, int id, int numPassengers, Route route) {
+    public Stop(String name, int id, int numPassengers, Point location) {
         super(name, id);
         this.numPassengers = numPassengers;
-        this.route = route;
+        this.location = location;
+    }
+
+    public void setLocation(Point p) {
+        location = p;
+    }
+
+    public Point getLocation() {
+        return location;
     }
 }
