6ba537c2c13082e7a778659d50fef72cd87d3295
Ravi, Nishant <nravi34@gatech.edu>
2020-06-28 14:33:25 -0700

    Added location and getters

--- src/Bus.java
+++ src/Bus.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Bus extends SimulationObject {
 
     private int numPassengers;
@@ -5,14 +7,24 @@ public class Bus extends SimulationObject {
     private Route route;
     private Stop currStop;
     private Stop nextStop;
+    private Point location;
 
     public Bus(String name, int id, int numPassengers, double avgSpeed,
-               Route route, Stop currStop, Stop nextStop) {
+               Route route, Stop currStop, Stop nextStop, Point location) {
         super(name, id);
         this.numPassengers = numPassengers;
         this.avgSpeed = avgSpeed;
         this.route = route;
         this.currStop = currStop;
         this.nextStop = nextStop;
+        this.location = location;
+    }
+
+    public void setLocation(Point p) {
+        location = p;
+    }
+
+    public Point getLocation() {
+        return location;
     }
 }
