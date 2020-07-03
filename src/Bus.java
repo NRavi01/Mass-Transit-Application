4289cfe89cbd42bdeb6fb2060e1956e76059ebd3
Ravi, Nishant <nravi34@gatech.edu>
2020-07-03 14:31:41 -0700

    Added relative location bus

--- src/Bus.java
+++ src/Bus.java
@@ -8,6 +8,7 @@ public class Bus extends SimulationObject {
     private Stop currStop;
     private Stop nextStop;
     private Point location;
+    private Point screenLocation;
 
     public Bus(String name, int id, int numPassengers, double avgSpeed,
                Route route, Stop currStop, Stop nextStop, Point location) {
@@ -18,6 +19,7 @@ public class Bus extends SimulationObject {
         this.currStop = currStop;
         this.nextStop = nextStop;
         this.location = location;
+        this.screenLocation = location;
     }
 
     public void setLocation(Point p) {
@@ -26,5 +28,13 @@ public class Bus extends SimulationObject {
 
     public Point getLocation() {
         return location;
+    }
+
+    public void setScreenLocation(Point p) {
+        screenLocation = p;
+    }
+
+    public Point getScreenLocation() {
+        return screenLocation;
     }
 }
