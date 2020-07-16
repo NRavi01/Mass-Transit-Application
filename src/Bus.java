60af23376485028e38359d945e9895c3faad6f9e
Ravi, Nishant <nravi34@gatech.edu>
2020-07-16 01:42:23 -0700

    Bus integration

--- src/Bus.java
+++ src/Bus.java
@@ -5,12 +5,15 @@ public class Bus extends SimulationObject {
     private int numPassengers;
     private double avgSpeed;
     private Route route;
+    private int currentStop;
     private Stop currStop;
     private Stop nextStop;
     private Point location;
     private Point screenLocation;
     private int currFuel;
     private int fuelCapacity;
+    private int passengerCapacity;
+    boolean outbound;
 
     public Bus(String name, int id, int numPassengers, double avgSpeed,
                Route route, Stop currStop, Stop nextStop, Point location, int currFuel, int fuelCapacity) {
@@ -24,6 +27,64 @@ public class Bus extends SimulationObject {
         this.screenLocation = location;
         this.currFuel = currFuel;
         this.fuelCapacity = fuelCapacity;
+    }
+    public Bus(String id, Route route, boolean outbound, int currentStop, double latitude, double longitude, int passengers,
+               int passengerCapacity, double fuel, double fuelCapacity, double speed) {
+        super("Bus", Integer.valueOf(id));
+        this.numPassengers = numPassengers;
+        this.avgSpeed = avgSpeed;
+        this.outbound = outbound;
+        this.route = route;
+        this.currentStop = currentStop;
+        this.nextStop = route.getStops().get(currentStop + 1);
+        this.location = new Point((int) latitude, (int) longitude);
+        this.screenLocation = location;
+        this.currFuel = (int) fuel;
+        this.fuelCapacity = (int) fuelCapacity;
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public boolean isOutbound() {
+        return outbound;
+    }
+
+    public int getOutboundAsInt() {
+        return outbound ? 0 : 1;
+    }
+
+    public void setOutbound(boolean outbound) {
+        this.outbound = outbound;
+    }
+
+    public void setOutbound(int outbound) {
+        this.outbound = outbound == 0;
+    }
+
+    public int getCurrentStopIndex() {
+        return currentStop;
+    }
+
+    public void setCurrentStopIndex(int currentStop) {
+        if (currentStop < -1) {
+            throw new RuntimeException("can't set current stop to any value less than -1");
+        } else if (currentStop != -1) {
+            if (route == null) {
+                throw new RuntimeException("can't set current stop to value other than -1 when the bus's route is null");
+            } else if (currentStop >= route.getStops().size()) {
+                throw new IndexOutOfBoundsException("can't set current stop to value greater than or equal to length of stops on bus's route");
+            }
+        }
+        this.currentStop = currentStop;
+    }
+
+    /**
+     * @return the stop the bus is at currently or the last stop the bus visited
+     */
+    public Stop getCurrentStop() {
+        if (currentStop == -1 || route == null) {
+            return null;
+        }
+        return route.getStops().get(currentStop);
     }
 
     public void setLocation(Point p) {
@@ -78,6 +139,22 @@ public class Bus extends SimulationObject {
         return nextStop;
     }
 
+    public double getFuel() {
+        return currFuel;
+    }
+
+    public double getSpeed() {
+        return avgSpeed;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
     public void setNextStop(Stop nextStop) {
         this.nextStop = nextStop;
     }
@@ -103,6 +180,24 @@ public class Bus extends SimulationObject {
         if (numPassengers < 0) {
             numPassengers = 0;
         }
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "('%s', '%s', %d, %d, %f, %f, %d, %d, %d, %d, %f)",
+                getID(),
+                (route == null ? "null" : route.getID()),
+                outbound ? 0 : 1,
+                currentStop,
+                location.getX(),
+                location.getY(),
+                numPassengers,
+                passengerCapacity,
+                currFuel,
+                fuelCapacity,
+                avgSpeed
+        );
     }
 
 }
